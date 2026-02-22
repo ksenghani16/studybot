31ac50bf325e47b1c01895787e8750df25132a4c
ksenghani16 <ksenghani16@student.sfit.ac.in>
2026-02-22 15:23:31 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from pymongo import MongoClient
+from datetime import datetime, timezone
+from langchain_core.messages import HumanMessage, AIMessage
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+mongo_uri=os.getenv("MONGODB_URI")
+client=MongoClient(mongo_uri)
+db=client["chat"]
+collection=db["users"]
+app=FastAPI()
+class ChatRequest(BaseModel):
+    user_id: str
+    question: str
+
+app.add_middleware(
+    CORSMiddleware,allow_origins=["*"],allow_methods=["*"],allow_headers=["*"],allow_credentials=True)
+prompt=ChatPromptTemplate.from_messages([
+    ("system","""
+You are StudyBot.
+Teach like a teacher.
+Explain topics simply.
+Remember user's personal details if they share.
+If user asks about their name, answer from memory.
+"""),
+    ("placeholder","{history}"),
+    ("user","{question}")
+])
+llm=ChatGroq(api_key=groq_api_key,model="openai/gpt-oss-20b")
+chain=prompt | llm
+
+def get_history(user_id):
+    chats=collection.find({"user_id":user_id}).sort("timestamp",1)
+    history=[]
+    for chat in chats:
+        history.append((chat["role"],chat["message"]))
+    return history
+@app.get("/")
+def home():
+    return {"message":"Welcome to StudyBot API"}
+@app.post("/chat")
+def chat(request:ChatRequest):
+        history=get_history(request.user_id)
+        response=chain.invoke({"history":history,"question":request.question})
+        collection.insert_one({"user_id":request.user_id,"role":"user","message":request.question,"timestamp":datetime.now(timezone.utc)})
+        collection.insert_one({"user_id":request.user_id,"role":"assistant","message":response.content,"timestamp":datetime.now(timezone.utc)})
+        return {"response":response.content}
+
+
+        
